33d4ab8c6248ce3f15da28fb6ede9750f8039079
Migu3e <itaymigul@gmail.com>
2024-05-20 17:35:56 +0300

    fix all the deleted files - Register UI and Reset UI

---
+++ Client/Views/ForgotPasswordView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client.Views
+{
+    /// <summary>
+    /// Interaction logic for ForgotPasswordView.xaml
+    /// </summary>
+    public partial class ForgotPasswordView : Window
+    {
+        public ForgotPasswordView()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var loginPage = new LoginView();
+            loginPage.Show();
+            this.Close();
+
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
