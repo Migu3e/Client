00b7296e949d6f34cdec32e4efecaa893ef59d7d
Migu3e <itaymigul@gmail.com>
2024-05-21 12:59:06 +0300

    Menu Side Bar

--- Client/Views/MainWindow.xaml.cs
+++ Client/Views/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//can move
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
 
 namespace Client
 {
@@ -23,6 +28,16 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
         }
     }
 }
